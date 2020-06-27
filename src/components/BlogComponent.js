c084ea674720c58154e80315facc4bdc81bde7b2
SUYASH15012001 <sakshams1501@gmail.com>
2020-06-28 02:46:35 +0530

    Multiple files can be uploaded

--- src/components/BlogComponent.js
+++ src/components/BlogComponent.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { createBlog } from '../redux/actions/blogActions';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import firebase from '../config/fbConfig'
 
 class CreateBlog extends Component {
     state={
         title:'',
         subtitle:'',
         content: '',
-        authorName: ''
+        authorName: '',
+        img: [],
+        files: []
     };
 
     //e is the event i.e. is passed to the event handler fucntion.
@@ -18,13 +21,48 @@ class CreateBlog extends Component {
         })
     }
 
+    handleChangeImage = (file) => { 
+      this.setState({
+        files: [...this.state.files , ...file]
+      })
+    }
+
+    handleSaveImage = () => {
+      let bucketName = 'images';
+      this.state.files && this.state.files.map( (file) => {
+        let storageRef = firebase.storage().ref(`${bucketName}/${file.name}`);
+        let uploadTask = storageRef.put(file)
+        uploadTask.on( firebase.storage.TaskEvent.STATE_CHANGED,() => 
+        {
+          let downloadURL = uploadTask.snapshot.downloadURL;
+        })  
+      });
+    }
+
+    imgPush = () => {
+      this.state.files && this.state.files.map((file) => {
+        let storageRef = firebase.storage().ref();
+        let spaceRef = storageRef.child('images/'+file.name)
+        storageRef.child('images/'+file.name).getDownloadURL().then((url) => {
+          this.setState({
+            img:[...this.state.img, url]
+          })
+        })
+      })
+      this.setState({
+        files: []
+      })
+      console.log('it should have images',this.state)
+    }
 
     handleSubmit = (e) => {
         e.preventDefault();
-        // console.log(this.state);
-        this.props.createBlog(this.state);
-        this.props.history.push('/');//to redirect afetr the project has been made..........
-    }
+        console.log("Form submit",this.state);
+        this.handleSaveImage();
+        setTimeout(() => this.imgPush(),1000);
+        setTimeout(() => this.props.createBlog(this.state),2000);
+        setTimeout(() => this.props.history.push('/') , 4000) ;
+      }
     render() {
         return (
             <Form onSubmit={this.handleSubmit}>
@@ -44,6 +82,10 @@ class CreateBlog extends Component {
                 <FormGroup className='row row-content'>
                   <Label for="authorName" className='col-12 col-md-2'>Author Name</Label>
                   <Input type="text" name="authorName" id="authorName" onChange={this.handleChange}   placeholder="Enter your name" className='col-12 col-md-9 offset-sm-1'/>
+                </FormGroup>
+                <FormGroup className='row row-content'>
+                  <Label for="files" className='col-12 col-md-2'>Images</Label>
+                  <input type="file" multiple name="files" id="files" onChange={(e) => {this.handleChangeImage(e.target.files)}} placeholder="Enter your name" className='col-12 col-md-9 offset-sm-1'/>
                 </FormGroup>
                 <Button className='btn btn-secondary' type='submit'>Submit</Button>
               </div>
