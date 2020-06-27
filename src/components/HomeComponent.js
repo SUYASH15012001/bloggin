f496d5334a67b8f48a20672148ab6c05b9cbe221
SUYASH15012001 <sakshams1501@gmail.com>
2020-06-27 22:41:47 +0530

    Lets get started to publish images to our blogs

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle , CardFooter} from 'reactstrap';
 import {connect} from 'react-redux';
-
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 
 function RenderCard({blogs}) {
     return(
@@ -11,11 +12,16 @@ function RenderCard({blogs}) {
                 return (
                     <div className="col-12 col-sm-10 offset-sm-1 mt-4" key={blog.id}>
                         <Card>
-                            <CardImg height='400px' src= 'https://firebasestorage.googleapis.com/v0/b/blog-e25f9.appspot.com/o/images%2Futhappizza.png?alt=media&token=98b6e4e5-2778-423a-9a7f-a16afc6b9d4c'  alt='Card image' />
+                            {blog.img && blog.img.map(blogImg => {
+                                return (
+                                    <CardImg key= {blog.img.indexOf(blogImg)} height='400px' 
+                                    src= {blogImg} alt='Card image' />
+                                )})
+                            }
                             <CardBody>
                                 <CardTitle> {blog.title} </CardTitle>
                                 <CardSubtitle> {blog.subtitle} </CardSubtitle>
-                                <CardText> {blog.description} </CardText>
+                                <CardText> {blog.content} </CardText>
                                 <CardFooter> {blog.authorName}  </CardFooter>
                             </CardBody>
                         </Card>
@@ -29,7 +35,6 @@ function RenderCard({blogs}) {
 
 class HomeComponent extends Component {
     render() {
-        console.log(this.props);
         return (
             <div className="container">
                 <RenderCard blogs={this.props.blogs} />
@@ -40,8 +45,13 @@ class HomeComponent extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        blogs: state.blog.blogs
+        blogs: state.firestore.ordered.blogs
     }
 }
 
-export default connect(mapStateToProps)(HomeComponent);
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        { collection: 'blogs' }
+    ])
+)(HomeComponent);
--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import blogReducer from './blogReducer';
 import contactReducer from './contactReducer';
 import authReducer from './authReducer';
 import { combineReducers } from 'redux';
+import { firestoreReducer } from 'redux-firestore';
 
 const rootreducer = combineReducers({
     auth: authReducer,
     blog: blogReducer,
-    contact: contactReducer
+    contact: contactReducer,
+    firestore: firestoreReducer
 })
 
 export default rootreducer
